528e1f35a6e7bd358e13603ece7ba2923eb66356
punieth <36213819+punieth@users.noreply.github.com>
2026-08-12 13:47:42 +0530

    feat: migrate from next-on-pages to @opennextjs/cloudflare for Workers deployment

---
+++ open-next.config.ts
@@ -0,0 +1,22 @@
+import cache from "@opennextjs/cloudflare/kvCache";
+
+export default {
+  default: {
+    override: {
+      wrapper: "cloudflare-node",
+      converter: "edge",
+      incrementalCache: "dummy",
+      tagCache: "dummy",
+      queue: "dummy",
+    },
+  },
+
+  middleware: {
+    external: true,
+    override: {
+      wrapper: "cloudflare-edge",
+      converter: "edge",
+      proxyExternalRequest: "fetch",
+    },
+  },
+};
